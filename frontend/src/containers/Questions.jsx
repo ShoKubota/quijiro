b2cbff67ab667b36f28e098c1eee06903f84d3db
ShoKubota <sh2o.1114@mineo.jp>
2022-05-19 17:14:06 +0900

    apiを叩く動作確認

--- frontend/src/containers/Questions.jsx
+++ frontend/src/containers/Questions.jsx
@@ -1,6 +1,17 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
+
+//apis
+import { fetchQuestions } from '../apis/questions';
 
 export const Questions = () => {
+
+  useEffect(() => {
+    fetchQuestions()
+    .then((data) =>
+      console.log(data)
+    )
+  }, [])
+
   return(
     <>
       問題一覧
