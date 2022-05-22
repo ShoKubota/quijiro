f726c4095d994419b62e633c1c775c7a825f8815
ShoKubota <sh2o.1114@mineo.jp>
2022-05-22 18:10:38 +0900

    デフォのURLをかえる

--- frontend/src/urls/index.js
+++ frontend/src/urls/index.js
@@ -1,3 +1,3 @@
-const DEFAULT_API_LOCALHOST = 'http://localhost:3000/api/v1'
+const DEFAULT_API_LOCALHOST = 'https://quijiro.herokuapp.com/api/v1'
 
 export const questionIndex = `${DEFAULT_API_LOCALHOST}/questions`
